1300629a2ac7ba313431f706e50ce399d7dc043b
Billy Mackay <bossmack50@gmail.com>
2019-07-22 16:31:37 +0100

    Added set_coach_name

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -1,9 +1,27 @@
 require("minitest/autorun")
 require("minitest/rg")
-require_relative("../classes.rb")
+require_relative("../sports_team.rb")
 
 class TestStudent < MiniTest::Test
 
   def setup()
-    @student = Student.new("Billy", "G14")
+    @team = Team.new("Leggy Ball Men", ["Billy", "Jimmy", "Bob"], "Coach big legs")
   end
+
+  def test_get_team_name()
+    assert_equal("Leggy Ball Men", @team.team_name)
+  end
+
+  def test_get_players()
+    assert_equal(["Billy", "Jimmy", "Bob"], @team.players)
+  end
+
+  def test_get_coach()
+    assert_equal("Coach big legs", @team.coach_name)
+  end
+
+  def test_set_coach_name()
+    @team.set_coach_name("Coach small legs")
+    assert_equal("Coach small legs", @team.coach_name)
+  end
+end
---
+++ sports_team.rb
@@ -0,0 +1,25 @@
+class Team
+
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+  end
+
+  def team_name()
+    return @team_name
+  end
+
+  def players()
+    return @players
+  end
+
+  def coach_name()
+    return @coach
+  end
+
+  def set_coach_name(name)
+    @coach = name
+  end
+
+end
